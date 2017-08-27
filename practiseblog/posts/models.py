3f10c0966e0b9b7ddc59b1810cf1aaccbf2d6a1e
Angela Maugey <a.maugey@gmail.com>
2017-08-27 20:21:16 +0100

    creating a model for posts

--- practiseblog/posts/models.py
+++ practiseblog/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='assets/')
+    body = models.TextField()
