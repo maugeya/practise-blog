2ca17ee4cdabff04a2196342a31d557f31a2216f
Angela Maugey <a.maugey@gmail.com>
2017-08-27 22:11:52 +0100

    accessing post details to show on page. Using object_or_404

--- practiseblog/practiseblog/urls.py
+++ practiseblog/practiseblog/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$' , views.home ),
-    url(r'^posts/(?P<post_id>[0-9]+)/$' , views.posts_details ),
+    url(r'^posts/(?P<post_id>[0-9]+)/$' , views.posts_details, name="posts_details" ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
