539fdd726edc33237f547cc7ab4b298531feca84
Angela Maugey <a.maugey@gmail.com>
2017-08-31 16:24:42 +0100

    adding about page

--- practiseblog/practiseblog/urls.py
+++ practiseblog/practiseblog/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from posts import views
+import posts.views
+import sitepages.views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$' , views.home ),
-    url(r'^posts/(?P<post_id>[0-9]+)/$' , views.posts_details, name="posts_details" ),
+    url(r'^$' , posts.views.home, name="home" ),
+    url(r'^posts/(?P<post_id>[0-9]+)/$' , posts.views.posts_details, name="posts_details" ),
+    url(r'^about/' , sitepages.views.about, name="about" ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
